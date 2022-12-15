c82973ffd5d230642902c8966906d014dc270237
Jack McCalley <34116924+JackMcCalley@users.noreply.github.com>
2022-12-15 12:50:07 -0800

    basic styling

--- components/MatchCard.js
+++ components/MatchCard.js
@@ -7,15 +7,35 @@ export const MatchCard = (matchData) => {
     <div>
       {data[0].winPlace == 1 ? <h1>WINNER!</h1> : <div></div>}
       <h1>Team Rank: {data[0].winPlace}</h1>
-      {data.map((player) => {
+      <div style={styles.grid}>
+        {data.map((player) => {
           return (
-            <div key={player.name} style={{borderStyle: 'solid', borderWidth: '1px'}}>
+            <div key={player.name} style={styles.player}>
               <h1>{player.name}</h1>
               <h2>Kills: {player.kills}</h2>
               <h2>Damage: {player.damageDealt}</h2>
             </div>
           );
-      })}
+        })}
+      </div>
     </div>
   );
+};
+
+const styles = {
+  grid: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "90vw",
+  },
+  player: {
+    display: 'inline-block',
+    alignItems: 'center',
+    width: "25vmax",
+    height: "auto",
+    borderStyle: "solid",
+    borderWidth: "1px",
+    margin: '10px'
+  },
 };
